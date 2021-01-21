d430e6d93b8d03298b1b90f115aac4511c5eb56c
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-01-21 19:50:51 +0530

    Error and datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   imports: [],
-  exports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
 export class MaterialModule {}
