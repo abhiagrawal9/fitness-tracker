6a04422fc1629dbbf3b3a3a9412c2d63da50ac5d
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-01-21 16:57:41 +0530

    added material and implemented basic sign up page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignupComponent,
+    // component: WelcomeComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'training',
+    component: TrainingComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  imports: [],
+  exports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+})
+export class MaterialModule {}
