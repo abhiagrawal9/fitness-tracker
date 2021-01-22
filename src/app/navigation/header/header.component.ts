59ef91c62ebee2af17e285179031d68f45a14a90
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-01-22 22:38:03 +0530

    brokedown the app component into two child components

---
+++ src/app/navigation/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  @Output() toggleSideNav = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onToggleSidenav(): void {
+    this.toggleSideNav.emit();
+  }
+}
