80e2db5e9d1082d78279a68a790b775e1c7a84e8
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-01-24 17:21:01 +0530

    route guard for training page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { TrainingComponent } from './training/training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    // component: LoginComponent,
     component: WelcomeComponent,
   },
   {
@@ -26,20 +23,7 @@ const routes: Routes = [
   {
     path: 'training',
     component: TrainingComponent,
-    // children: [
-    //   {
-    //     path: 'new',
-    //     component: NewTrainingComponent,
-    //   },
-    //   {
-    //     path: 'history',
-    //     component: PastTrainingsComponent,
-    //   },
-    //   {
-    //     path: ':id',
-    //     component: CurrentTrainingComponent,
-    //   },
-    // ],
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
