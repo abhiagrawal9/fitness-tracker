ca1d81c5720bfeb95b2b0a15b5e9dd9a341dd176
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-01-24 10:57:59 +0530

    Sidenav auth effect

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,18 +1,39 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss'],
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
+  isAuth = false;
+  authSub: Subscription;
   @Output() closeSideNav = new EventEmitter<void>();
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authSub = this.authService.authChange.subscribe(
+      (isAuthenticated: boolean) => {
+        this.isAuth = isAuthenticated;
+      }
+    );
+  }
 
   onCloseSidenav(): void {
     this.closeSideNav.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
   }
 }
