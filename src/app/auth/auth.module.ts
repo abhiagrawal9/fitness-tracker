559d3eacd4349db5c96e326f9f851e1396ac985a
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-01-27 20:00:03 +0530

    Update auth.module.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -13,6 +14,7 @@ import { MaterialModule } from '../material.module';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    AngularFireAuthModule,
     MaterialModule,
     FlexLayoutModule,
   ],
