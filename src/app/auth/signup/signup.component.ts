a3cad534faa9f5195581c25fcdd659aacfb9d538
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-01-27 21:28:09 +0530

    removed the above 18 check while signing up

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -12,14 +12,14 @@ import { UiService } from 'src/app/shared/ui.service';
 })
 export class SignupComponent implements OnInit, OnDestroy {
   private loadingStateSub: Subscription;
-  maxDate: Date;
+  // maxDate: Date;
   isLoading = false;
 
   constructor(private authService: AuthService, private uiService: UiService) {}
 
   ngOnInit(): void {
-    this.maxDate = new Date();
-    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    // this.maxDate = new Date();
+    // this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
     this.loadingStateSub = this.uiService.loadingStateChanged.subscribe(
       (loadingState: boolean) => {
         this.isLoading = loadingState;
